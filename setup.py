a2e3711e71d36d9b4598724e37f647e8d2d23df4
Marcelo Canina <me@marcanuy.com>
2019-03-08 16:47:55 -0300

    add license to setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',  # example license
+    license='MIT License',  # example license
     description='A simple Django app to generate pages from files.',
     long_description=README,
     url='https://github.com/marcanuy/django-staticpages/',
@@ -23,7 +23,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.5',
